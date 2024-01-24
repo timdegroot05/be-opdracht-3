d75745a026adb5ec2d8f2aadeb06a21b41284d45
timdegroot05 <336795@student.mboutrecht.nl>
2024-01-24 10:48:47 +0100

    commit

--- app/config/config.php
+++ app/config/config.php
@@ -3,6 +3,6 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'BE-J2-OP3');
+define('DB_NAME', 'Backenedop3');
 define('URLROOT', 'http://be-opdracht3/');
  
